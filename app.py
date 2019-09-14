62ce74532be1e637cd8ceaa584a23b534624757a
Nicolai Negru <negru.n@gmail.com>
2019-09-14 17:52:08 +0100

    Add dummy index.html page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
-from flask import Flask, request, url_for, redirect
+from flask import Flask, request, url_for, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Working...'
+    return render_template('index.html')
 
 if __name__ == "__main__":
     host = os.environ.get('IP', '0.0.0.0')
