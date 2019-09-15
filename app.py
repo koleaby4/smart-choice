11ff8ca9307db0659db95e83bc4016fb46b59d29
Nicolai Negru <negru.n@gmail.com>
2019-09-15 16:32:39 +0100

    Add rules.html

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/rules')
+def rules():
+    return render_template('rules.html')
+
 if __name__ == "__main__":
     host = os.environ.get('IP', '0.0.0.0')
     port = os.environ.get('PORT', 5000)
