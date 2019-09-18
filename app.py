3a0215195631a293e172750a1316895e5a678c0e
Nicolai Negru <negru.n@gmail.com>
2019-09-18 15:43:53 +0100

    Add core functionality for creating rules

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, request, url_for, redirect, render_template
 import mongo_helpers
+import datetime
+import json
 
 app = Flask(__name__)
 
@@ -15,13 +17,54 @@ def rules():
     return render_template('rules.html', rules=mongo_helpers.get_rules())
 
 
-@app.route('/rules/create')
-def create_rule():
-    import json
-    data = json.loads(open('rule.json').read())
-    print(data)
-    mongo_helpers.insert_rule(data)
+def get_rule_payload(payload):
+    data = {'date_created': str(datetime.datetime.now())}
+    data['rule_name'] = payload['rule_name']
+
+    criteria = []
+    multipliers = []
+    notes = []
+
+    for key in payload:
+        value = payload[key]
+        if key.startswith('name'):
+            criteria.append(value)
+        elif key.startswith('multiplier'):
+            multipliers.append(int(value))
+        elif key.startswith('note'):
+            notes.append(value)
+
+    # ToDo: remove once done
+    print(criteria)
+    print(multipliers)
+    print(notes)
+
+    criteria_entries = []
+
+    for i in range(len(criteria)):
+        criteria_entries.append(
+            {
+                "criterion": criteria[i],
+                "multiplier": multipliers[i],
+                "note": notes[i]
+            }
+        )
+
+    data['criteria'] = criteria_entries
     return data
+
+
+@app.route('/rules/create', methods=['GET', 'POST'])
+def create_rule():
+    if request.method == 'POST':
+        # ToDo: remove
+        print(json.dumps(request.form))
+        data = get_rule_payload(request.form.to_dict().copy())
+        print(json.dumps(data))
+        mongo_helpers.insert_rule(data)
+        return redirect(url_for('rules'))
+    else:
+        return render_template('create_rule.html')
 
 
 if __name__ == "__main__":
