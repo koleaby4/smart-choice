edfdf8b5338c3e85698fd34ef6840d166e0c5919
Nicolai Negru <negru.n@gmail.com>
2020-05-17 06:57:49 +0100

    Moved inlined JS from rules.html into rules.js

---
+++ static/rules.js
@@ -0,0 +1,35 @@
+/* Java Script for rules.html */
+
+document.getElementById("createRuleButton").onclick = () => location.href = `/rules/0`
+
+function openRuleDetails(rule_id) {
+    location.href = `/rules/${rule_id}`
+}
+
+function deleteRuleFromDatabase(rule_id) {
+    console.log(`trying to delete ${rule_id}`)
+    let outcome
+
+    let url = `/rules/${rule_id}`
+    console.log(url)
+    fetch(url, { method: 'delete' })
+        .then(response => response.json())
+        .then(outcome => console.log(outcome))
+}
+
+/* delete rule from UI and DB */
+function deleteRule(event) {
+    {
+        const row = event.closest("tr")
+        console.log('deleting closest...')
+        deleteRuleFromDatabase(row.id)
+        row.remove()
+    }
+}
+
+function editRule(event) {
+    {
+        const ruleRow = event.closest("tr")
+        location.href = `/rules/${ruleRow.id}`
+    }
+}
